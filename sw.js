3abe489b23eaff67a45ce2382ab7ec5d72078b60
louisdelange2-max <louisdelange2@gmail.com>
2026-05-05 21:55:14 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ppe-issue-record-v4-offline-buttons';
+const CACHE='ppe-issue-record-v4.1-offline-buttons';
 const CORE=['./','index.html','manifest.json','sw.js','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
